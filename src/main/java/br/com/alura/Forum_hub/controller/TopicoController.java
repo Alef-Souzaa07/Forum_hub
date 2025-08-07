9e7bfa464c1fea75f28878ff199d8757ff239b20
Alef-Souzaa07 <alef.programer@gmail.com>
2025-08-07 17:43:32 -0300

    Implementando o save dos dados do registro

--- src/main/java/br/com/alura/Forum_hub/controller/TopicoController.java
+++ src/main/java/br/com/alura/Forum_hub/controller/TopicoController.java
@@ -2,7 +2,9 @@ package br.com.alura.Forum_hub.controller;
 
 import br.com.alura.Forum_hub.domain.topico.DadosRegistroTopico;
 import br.com.alura.Forum_hub.domain.topico.Topico;
+import br.com.alura.Forum_hub.domain.topico.TopicoRespository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/topicos")
 public class TopicoController {
 
+    @Autowired
+    private TopicoRespository respository;
 
     @PostMapping
-    public void RegistrarTopico(@RequestBody String json){
-        System.out.println(json);
+    public void RegistrarTopico(@RequestBody DadosRegistroTopico dados){
+        respository.save(new Topico(dados));
     }
 }
