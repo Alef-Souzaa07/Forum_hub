00334a58116e8acdb3930887a9c26e5d6616f094
Alef-Souzaa07 <alef.programer@gmail.com>
2025-08-07 17:04:12 -0300

    Tirando o stacktrace do erro na requisição

--- src/main/java/br/com/alura/Forum_hub/controller/TopicoController.java
+++ src/main/java/br/com/alura/Forum_hub/controller/TopicoController.java
@@ -4,6 +4,7 @@ import br.com.alura.Forum_hub.domain.topico.DadosRegistroTopico;
 import br.com.alura.Forum_hub.domain.topico.Topico;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicoController {
 
 
-
+    @PostMapping
     public void RegistrarTopico(@RequestBody String json){
         System.out.println(json);
     }
