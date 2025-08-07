0d38a24c857d67b26118ffd2b1917459c7aff24a
Alef-Souzaa07 <alef.programer@gmail.com>
2025-08-07 17:34:58 -0300

    Implementado a classe que me permite nao precisar de autenticacao para requisicao

---
+++ src/main/java/br/com/alura/Forum_hub/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.alura.Forum_hub.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults()) // forma recomendada agora
+                .csrf(csrf -> csrf.disable());        // forma recomendada agora
+
+        return http.build();
+    }
+}
+
