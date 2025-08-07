bb89bdd837e88be5bc84f8e224498ab655190fc2
Alef-Souzaa07 <alef.programer@gmail.com>
2025-08-07 18:07:32 -0300

    Criando primeira versao migration

---
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -0,0 +1,10 @@
+create table topicos(
+    id bigint not null auto_increment,
+    titulo varchar(255) not null,
+    mensagem TEXT not null,
+    data DATETIME not null ,
+    status varchar(100) not null ,
+    autor varchar(100),
+    nome varchar(100),
+    categoria varchar(255)
+);
