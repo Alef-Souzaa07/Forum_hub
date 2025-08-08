7df86d24c98297cfd43fc53d4a085f9a29c8d66d
Alef-Souzaa07 <alef.programer@gmail.com>
2025-08-08 18:00:19 -0300

    Adicionando a identificacao de ID a listagem dos topicos

--- src/main/java/br/com/alura/Forum_hub/domain/topico/DadosListagemTopicos.java
+++ src/main/java/br/com/alura/Forum_hub/domain/topico/DadosListagemTopicos.java
@@ -5,8 +5,8 @@ import br.com.alura.Forum_hub.domain.curso.DadosDoCurso;
 
 import java.time.LocalDateTime;
 
-public record DadosListagemTopicos(String titulo, String mensagem, LocalDateTime data, EstadoTopico status, String autor, Curso curso) {
+public record DadosListagemTopicos(Long id, String titulo, String mensagem, LocalDateTime data, EstadoTopico status, String autor, Curso curso) {
     public DadosListagemTopicos(Topico topico){
-        this(topico.getTitulo(), topico.getMensagem(), topico.getData(), topico.getStatus(), topico.getAutor(), topico.getCurso());
+        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getData(), topico.getStatus(), topico.getAutor(), topico.getCurso());
     }
 }
