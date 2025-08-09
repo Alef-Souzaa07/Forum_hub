f15aa252d6ef1dd8ac6e3b476918042864280e62
Alef-Souzaa07 <alef.programer@gmail.com>
2025-08-09 18:03:45 -0300

    Atualizando SecurityConfig para exigir autenticação

--- src/main/java/br/com/alura/Forum_hub/configuration/SecurityConfig.java
+++ src/main/java/br/com/alura/Forum_hub/configuration/SecurityConfig.java
@@ -1,12 +1,18 @@
 package br.com.alura.Forum_hub.configuration;
 
+import br.com.alura.Forum_hub.autenticacao.FiltroJwt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.POST;
 
@@ -14,18 +20,28 @@ import static org.springframework.http.HttpMethod.POST;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private FiltroJwt jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        return http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/topicos/**").permitAll()
-                        .anyRequest().authenticated() // Opcional, se quiser bloquear outras rotas
+                        .requestMatchers("/auth/**").permitAll() // libera rota para login
+                        .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable());
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
-        return http.build();
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
+
 
 
 
