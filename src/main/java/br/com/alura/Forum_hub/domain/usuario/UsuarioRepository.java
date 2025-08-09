cdb5054fea9b2766022e1fa85f7c35044e4fa291
Alef-Souzaa07 <alef.programer@gmail.com>
2025-08-09 17:56:54 -0300

    Criado repositorio de usuario

---
+++ src/main/java/br/com/alura/Forum_hub/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.alura.Forum_hub.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByLogin(String login);
+}
+
