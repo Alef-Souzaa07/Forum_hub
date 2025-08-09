3cca1d92b290f86159778da14563535c308e1184
Alef-Souzaa07 <alef.programer@gmail.com>
2025-08-09 17:58:06 -0300

    Criado servico para carregar usuario e validar senha

---
+++ src/main/java/br/com/alura/Forum_hub/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,27 @@
+package br.com.alura.Forum_hub.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = repository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+        return User.builder()
+                .username(usuario.getLogin())
+                .password(usuario.getSenha()) // senha já deve estar hashada (ex: BCrypt)
+                .roles("USER")
+                .build();
+    }
+}
+
